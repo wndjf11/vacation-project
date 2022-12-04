45fd599567e47b72626305c52a1d475961651b1d
chanhee <wndjf11@naver.com>
2022-12-04 22:52:51 +0900

    1일차

---
+++ back-server/src/main/java/com/chance/project_introduce/ProjectIntroduceApplication.java
@@ -0,0 +1,12 @@
+package com.chance.project_introduce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectIntroduceApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectIntroduceApplication.class, args);
+	}
+
+}
---
+++ back-server/src/test/java/com/chance/project_introduce/ProjectIntroduceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chance.project_introduce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectIntroduceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
